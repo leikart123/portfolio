a161b333184020cc1039ce4ffd5d5d8c1d68e6b3
leikart123 <pgr97@mail.ru>
2021-01-09 09:41:08 +0300

    fixed

--- fractol_gpu/args_parse.c
+++ fractol_gpu/args_parse.c
@@ -10,6 +10,7 @@ void	print_usage(void)
 
 int		args_parse(int argc, char **argv, c_cntrl *cmd)
 {
+	cmd->type = -1;
 	if (argc != 2)
 	{
 		print_usage();
@@ -21,6 +22,8 @@ int		args_parse(int argc, char **argv, c_cntrl *cmd)
 			cmd->type = JULIA;
 		if (ft_strequ(argv[1], "mandelbrot\0"))
 			cmd->type = MANDELBROT;
+		if (cmd->type < 0)
+			ft_putstr("wrong fractal name\n");
 	}
 	return (cmd->type < 0 ? -1 : 1);
 }
